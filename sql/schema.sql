4ecba27cd583677fcb968335423cac211ee464aa
Shubh Panchal <shubh.panchal@education.nsw.gov.au>
2024-11-21 19:22:15 +1100

    Added sql schema

---
+++ sql/schema.sql
@@ -0,0 +1,43 @@
+-- Schema for mReviews
+CREATE TABLE IF NOT EXISTS mReviews (
+    id INTEGER PRIMARY KEY,
+    user TEXT,
+    stars INTEGER,
+    comment TEXT,
+    movieID INTEGER,
+    timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
+-- Schema for users
+CREATE TABLE IF NOT EXISTS users (
+    userID INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    email TEXT,
+    password TEXT
+);
+
+-- Schema for movies
+CREATE TABLE IF NOT EXISTS movies (
+    movieID INTEGER PRIMARY KEY,
+    movieName TEXT,
+    genre TEXT,
+    year INTEGER
+);
+
+-- Schema for gReviews
+CREATE TABLE IF NOT EXISTS gReviews (
+    id INTEGER PRIMARY KEY,
+    user TEXT,
+    stars INTEGER,
+    comment TEXT,
+    gameID INTEGER,
+    timestamp DATETIME DEFAULT (datetime('now', 'localtime'))
+);
+
+-- Schema for games
+CREATE TABLE IF NOT EXISTS games (
+    gameID INTEGER PRIMARY KEY,
+    gameName TEXT,
+    genre TEXT,
+    year INTEGER
+);
