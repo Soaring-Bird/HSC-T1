120ca9d260ee22c03fb7c526ece5cba019f8c239
Soaring-Bird <shubh.panchal@education.nsw.gov.au>
2024-11-23 21:05:25 +1100

    Message feedback for errors and successful entries

--- app.py
+++ app.py
@@ -24,12 +24,14 @@ def signup():
         password = request.form.get('password')
         username = request.form.get('username')
         if password != request.form.get('password_confirmation'): 
-            return render_template('signup.html')
+            return render_template('signup.html', message="Passwords do not match", category="error")
+        if query_db("SELECT 1 FROM users WHERE username = ?", (username,)):
+            return render_template('signup.html', message="Username already exists", category="error")
         hashed_password = sha1(password.encode('utf-8')).hexdigest()
         query_db("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", 
         (username, request.form.get('mail'), hashed_password), commit=True)
-        return render_template('signup.html')
-    return render_template('signup.html')
+        return render_template('signup.html', message="Account created successfully! Please log in.", category="success")
+    return render_template('signup.html', message=' ')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,9 +42,9 @@ def login():
         user = query_db("SELECT * FROM users WHERE username = ? AND password = ?", (username, hashed_password))
         if user:
             session['username'] = username
-            return render_template('login.html')
+            return render_template('login.html', message="Login successful", category="success")
         else:
-            return render_template('login.html')
+            return render_template('login.html', message="Invalid username or password", category="error")
     return render_template('login.html')
 
 @app.route('/logout')
@@ -59,23 +61,28 @@ def browse_reviews():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    if not session.get('username'): #will be modified for admin update
+        return render_template('add.html', message='You must be admin to add a movie.', category='error')
     if request.method == 'POST':
         query_db(f"INSERT INTO movies (movieName, genre, year) VALUES (?, ?, ?)",
                  (request.form['name'], request.form['genre'], request.form['year']), commit=True)
-        return render_template('add.html')
+        return render_template('add.html', message=f'Movie added!', category='success')
     return render_template('add.html')
 
 @app.route('/review/<int:id>')
 def review(id):
     title, year, genre = query_db("SELECT movieName, year, genre FROM movies WHERE movieID = ?", (id,))[0]
     reviews = query_db("SELECT * FROM mReviews WHERE movieID = ?", (id,))
-    return render_template('review.html', main=reviews, heading=f"{title}-{year}-{genre}",  item=id)
+    return render_template('review.html', main=reviews, heading=f"{title}-{year}-{genre}", 
+        message=request.args.get('message'), category=request.args.get('category'), item=id)
 
 @app.route('/addreview/<int:id>', methods=['POST'])
 def add_review(id):
+    if not session.get('username'): #in case added by url
+        return redirect(url_for('review', id=id, message='You must sign in to review!', category='error'))
     query_db(f"INSERT INTO mReviews (user, stars, comment, movieID) VALUES (?, ?, ?, ?)",
              (session['username'], int(request.form.get('stars')), request.form['comment'], id), commit=True)
-    return redirect(url_for('review', id=id))
+    return redirect(url_for('review', id=id, message="Review added!", category='success'))
 
 
 if __name__ == '__main__':
